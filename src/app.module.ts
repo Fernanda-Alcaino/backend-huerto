9e31a770f16556018b4309df73df4e821dd1a8a3
fernanda <fern.alcaino@duocuc.cl>
2025-12-14 14:33:49 -0300

    arreglo backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+// Módulos
 import { ProductsModule } from './products/products.module';
+import { SalesModule } from './sales/sales.module'; // 👈 Creado con comando
+import { UsersModule } from './users/users.module'; // 👈 Creado con comando
 import { AuthModule } from './auth/auth.module';
-import { SalesModule } from './sales/sales.module';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'root',
-            password: '',      // <--- Pon tu contraseña si tienes
-            database: 'huerto_db',
-            entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: true, // ¡IMPORTANTE! Esto crea las tablas automáticamente (Entities -> DB)
-        }),
-        ProductsModule,
-        AuthModule,
-        SalesModule,
-    ],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'tienda.db', // 👈 Se creará este archivo
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ProductsModule,
+    AuthModule,
+    SalesModule,
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
